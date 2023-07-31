e53079f96ad6c75ca3fbb7f1f215fedaf9965921
mh <maxhalverson@live.com>
2023-07-31 08:48:16 -0500

    init commit, created schema for db, installed inquirer

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'department',
+    },
+  ])
+  
