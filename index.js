b63da52a71638d2ffaad0ba4707dc639f545be2e
mh <maxhalverson@live.com>
2023-07-31 09:21:01 -0500

    set up switch case for prompt to choose which function to call

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-inquirer
-  .prompt([
-    {
-        name: 'menu',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"]
-    },
-  ])
-  
+const prompt = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'menu',
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"]
+            }
+        ]).then((res) => {
+            switch (res.menu) {
+                case "View All Departments":
+                    viewAllDepartments()
+                    break;
+                case "View All Roles":
+                    viewAllRoles()
+                    break;
+                case "View All Employees":
+                    viewAllEmployees()
+                    break;
+            }
+        })
+}
+
+const viewAllDepartments = () => {
+    console.log("test")
+    prompt()
+}
+
+const viewAllRoles = () => {
+    console.log("test")
+    prompt()
+}
+
+const viewAllEmployees = () => {
+    console.log("test")
+    prompt()
+}
+
+prompt()
